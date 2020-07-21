2fda1fa977a91298923a775cdec38ecd2473afca
Amy Fox <aef1004@gmail.com>
2020-07-21 11:31:17 -0600

    update description and add tidy_cfu man

--- R/tidy_cfu.R
+++ R/tidy_cfu.R
@@ -1,4 +1,14 @@
-# selects all columns that start with dilution
+#' Clean CFU data and selects all columns that start with dilution
+#'
+#' @param df dataframe that contains all of the raw CFU data with the dilutions
+#' @param CFU_column ?? not sure why I need this part actually...
+#'
+#' @return
+#' @export
+#'
+#' @examples tidy_CFU(CFU_excel_raw, "CFUs")
+#'
+#'
 tidy_CFU <- function(df, CFU_column) {
 
   CFU_column = as.name(CFU_column) # use this because I'm making changes to the column
