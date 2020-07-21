9420f55f4203fca29ad203f3e2716511b44fcd61
Amy Fox <aef1004@gmail.com>
2020-07-21 12:32:25 -0600

    add test and raw data

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Raw CFU data
+#'
+#' A dataset containing the Colony Forming Units (CFUs) for different mice
+#' at various dilutions
+#'
+#' @format A data frame with 46 rows and 7 variables:
+#' \describe{
+#'   \item{group}{group number to which each mouse belongs}
+#'   \item{mouse}{mouse name}
+#'   \item{organ}{organ name of each sample}
+#'   \item{dilution_0}{number of CFUs present for the undiluted sample}
+#'   \item{dilution_1}{number of CFUs present for dilution 1}
+#'   \item{dilution_2}{number of CFUs present for dilution 2}
+#'   \item{dilution_3}{number of CFUs present for dilution 3}
+#'   ...
+#' }
+#' @source
+"CFU_excel_raw"
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(bactcountr)
+
+test_check("bactcountr")
